332789c61a7a971f684cd45b0a7a07ecde17b9f5
Sam Nyashev <ll2.lol@yandex.ru>
2018-01-04 00:30:42 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include <Interface.h>
 
-#include "GarrysMod/Lua/Interface.h"
-
-using namespace GarrysMod;
+using namespace GarrysMod::Lua;
 
 #if _WIN32
-	#define POPEN _popen
-	#define PCLOSE _pclose
+#define POPEN _popen
+#define PCLOSE _pclose
 #else
-	#define POPEN popen
-	#define PCLOSE pclose
+#define POPEN popen
+#define PCLOSE pclose
 #endif
 
-int os_execute(lua_State *state) {
-	LUA->CheckType(1, Lua::Type::STRING);
+int os_execute(lua_State* state) {
+	LUA->CheckType(1, Type::STRING);
 
 	std::string shell = LUA->GetString(1);
 
@@ -42,11 +41,11 @@ int os_execute(lua_State *state) {
 }
 
 GMOD_MODULE_OPEN() {
-	LUA->PushSpecial(Lua::SPECIAL_GLOB);
-		LUA->GetField(-1, "os");
-			LUA->PushString("execute");
-			LUA->PushCFunction(os_execute);
-		LUA->SetTable(-3);
+	LUA->PushSpecial(SPECIAL_GLOB);
+	LUA->GetField(-1, "os");
+	LUA->PushString("execute");
+	LUA->PushCFunction(os_execute);
+	LUA->SetTable(-3);
 	LUA->Pop();
 
 	return 0;
